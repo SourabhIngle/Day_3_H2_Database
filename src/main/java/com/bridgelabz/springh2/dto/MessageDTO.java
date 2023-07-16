1c609ca4af3c275a93bc810178a99f64b2dea122
SourabhIngle <100rabhingle@gmail.com>
2023-07-16 23:26:51 +0530

    Store data in H2 database with multiple field

--- src/main/java/com/bridgelabz/springh2/dto/MessageDTO.java
+++ src/main/java/com/bridgelabz/springh2/dto/MessageDTO.java
@@ -2,6 +2,12 @@ package com.bridgelabz.springh2.dto;
 // DTO => transfer the data between the controller layer and the service layer
 
 
-public class MessageDTO {
+import java.time.LocalDate;
 
+public class MessageDTO {
+    public String title;
+    public String description;
+    public LocalDate startDate;
+    public String author;
+    public LocalDate endDate;
 }
--- src/main/java/com/bridgelabz/springh2/model/Message.java
+++ src/main/java/com/bridgelabz/springh2/model/Message.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.springh2.model;
 //model layer for the entity.
+
+import com.bridgelabz.springh2.dto.MessageDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import java.time.LocalDate;
 
 //with the help of @Entity it will create a table in database.
 // database name is = demoh2
@@ -14,14 +18,71 @@ public class Message {
     @Id              // This annotation is showing this my primary key
     @GeneratedValue  // Auto generate ids
     private int Id;
-    private String message;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private String author;
+    private LocalDate endDate;
 
-
-    public String getMessage() {
-        return message;
+    public Message() {
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public Message(MessageDTO messageDTO) {
+        this.updateMessage(messageDTO);
+    }
+
+    public void updateMessage(MessageDTO messageDTO){
+        this.title = messageDTO.title;
+        this.description = messageDTO.description;
+        this.startDate = messageDTO.startDate;
+        this.author = messageDTO.author;
+        this.endDate = messageDTO.endDate;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
     }
 }
